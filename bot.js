c6574b3287bd68afb9c2108af0640c03798fd115
Luffy-Jpg <138620598+Luffy-Jpg@users.noreply.github.com>
2025-04-09 23:35:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,64 +1,46 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
+/* Aura Bot - Powerful WhatsApp Userbot
+   Custom Build for Aura Project
+   Licensed under GPL-3.0 by Luffy-Jpg
 */
+
 const fs = require("fs")
 const path = require("path")
-const { handleMessages } = require("./Utilis/msg")
 const chalk = require("chalk")
+const got = require("got")
 const { DataTypes } = require("sequelize")
-const config = require("./config")
 const { WAConnection, MessageType } = require("@adiwajshing/baileys")
+
+const config = require("./config")
 const { StringSession } = require("./Utilis/whatsasena")
+const { handleMessages } = require("./Utilis/msg")
 const { getJson } = require("./Utilis/download")
 const { customMessageScheduler } = require("./Utilis/schedule")
 const { prepareGreetingMedia } = require("./Utilis/greetings")
 const { groupMuteSchuler, groupUnmuteSchuler } = require("./Utilis/groupmute")
 const { PluginDB } = require("./plugins/sql/plugin")
-
-// Sql
-const got = require("got")
 const { startMessage, waWebVersion } = require("./Utilis/Misc")
-const WhatsAsenaDB = config.DATABASE.define("WhatsAsena", {
-  info: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  value: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
+
+// Aura DB Init
+const AuraDB = config.DATABASE.define("AuraBot", {
+  info: { type: DataTypes.STRING, allowNull: false },
+  value: { type: DataTypes.TEXT, allowNull: false }
 })
 
+// Load SQL Plugins
 fs.readdirSync("./plugins/sql/").forEach((plugin) => {
-  if (path.extname(plugin).toLowerCase() == ".js") {
+  if (plugin.endsWith(".js")) {
     require("./plugins/sql/" + plugin)
   }
 })
 
-// Yalnızca bir kolaylık. https://stackoverflow.com/questions/4974238/javascript-equivalent-of-pythons-format-function //
+// Utility Formatters
 String.prototype.format = function () {
-  var i = 0,
-    args = arguments
-  return this.replace(/{}/g, function () {
-    return typeof args[i] != "undefined" ? args[i++] : ""
-  })
-}
-
-if (!Date.now) {
-  Date.now = function () {
-    return new Date().getTime()
-  }
+  var i = 0, args = arguments
+  return this.replace(/{}/g, () => typeof args[i] !== "undefined" ? args[i++] : "")
 }
 
 Array.prototype.remove = function () {
-  var what,
-    a = arguments,
-    L = a.length,
-    ax
+  var what, a = arguments, L = a.length, ax
   while (L && this.length) {
     what = a[--L]
     while ((ax = this.indexOf(what)) !== -1) {
@@ -68,120 +50,106 @@ Array.prototype.remove = function () {
   return this
 }
 
-async function whatsAsena(version) {
+async function auraBot(version) {
   await config.DATABASE.sync()
-  let StrSes_Db = await WhatsAsenaDB.findAll({
-    where: {
-      info: "StringSession",
-    },
-  })
+  let savedSession = await AuraDB.findAll({ where: { info: "StringSession" } })
   const conn = new WAConnection()
   conn.version = version
   const Session = new StringSession()
   conn.logger.level = config.DEBUG ? "debug" : "warn"
-  var nodb
 
-  if (StrSes_Db.length < 1 || config.CLR_SESSION) {
-    nodb = true
+  let noDbSession = false
+  if (savedSession.length < 1 || config.CLR_SESSION) {
+    noDbSession = true
     conn.loadAuthInfo(Session.deCrypt(config.SESSION))
   } else {
-    conn.loadAuthInfo(Session.deCrypt(StrSes_Db[0].dataValues.value))
+    conn.loadAuthInfo(Session.deCrypt(savedSession[0].dataValues.value))
   }
 
+  // Aura Connect Events
   conn.on("connecting", () => {
-    console.log(`${chalk.red.bgBlack("B")}${chalk.green.bgBlack(
-      "o"
-    )}${chalk.blue.bgBlack("t")}${chalk.yellow.bgBlack(
-      "t"
-    )}${chalk.white.bgBlack("u")}${chalk.magenta.bgBlack("s")}
-${chalk.white.bold.bgBlack("Version:")} ${chalk.red.bold.bgBlack(
-      config.VERSION
-    )}
-${chalk.blue.italic.bgBlack("ℹ️ Connecting to WhatsApp... Please wait.")}`)
+    console.log(chalk.magentaBright(`
+╭━━━╮╱╱╱╱╱╭╮
+┃╭━━╯╱╱╱╱╱┃┃
+┃╰━━┳━━┳━━┫┃╭┳━╮
+┃╭━━┫╭╮┃┃━┫╰╯┫╭╯
+┃┃╱╱┃╭╮┃┃━┫╭╮┫┃
+╰╯╱╱╰╯╰┻━━┻╯╰┻╯  v${config.VERSION}
+Connecting to Aura's WhatsApp bridge...
+`))
   })
+
   conn.on("open", async () => {
-    console.log(chalk.green.bold("✅ Login successful!"))
-    console.log(chalk.blueBright.italic("⬇️ Installing external plugins..."))
-    console.log(chalk.blueBright.italic("✅ Login information updated!"))
+    console.log(chalk.green.bold("✅ Aura Bot connected successfully!"))
 
     const authInfo = conn.base64EncodedAuthInfo()
-    if (StrSes_Db.length < 1) {
-      await WhatsAsenaDB.create({
-        info: "StringSession",
-        value: Session.createStringSession(authInfo),
-      })
+    if (savedSession.length < 1) {
+      await AuraDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) })
     } else {
-      await StrSes_Db[0].update({
-        value: Session.createStringSession(authInfo),
-      })
+      await savedSession[0].update({ value: Session.createStringSession(authInfo) })
     }
 
-    let plugins = await PluginDB.findAll()
-    plugins.map(async (plugin) => {
-      try {
-        if (!fs.existsSync("./plugins/" + plugin.dataValues.name + ".js")) {
-          console.log(plugin.dataValues.name)
+    // Load remote plugins
+    const plugins = await PluginDB.findAll()
+    for (let plugin of plugins) {
+      if (!fs.existsSync("./plugins/" + plugin.dataValues.name + ".js")) {
+        try {
           let response = await got(plugin.dataValues.url)
-          if (response.statusCode == 200) {
-            fs.writeFileSync(
-              "./plugins/" + plugin.dataValues.name + ".js",
-              response.body
-            )
+          if (response.statusCode === 200) {
+            fs.writeFileSync("./plugins/" + plugin.dataValues.name + ".js", response.body)
             require("./plugins/" + plugin.dataValues.name + ".js")
+            console.log(`⬇️  Loaded external plugin: ${plugin.dataValues.name}`)
           }
+        } catch {
+          console.log(`❌ Failed to load external plugin: ${plugin.dataValues.name}`)
         }
-      } catch (error) {
-        console.log(
-          `failed to load external plugin : ${plugin.dataValues.name}`
-        )
       }
-    })
-    console.log(chalk.blueBright.italic("⬇️  Installing plugins..."))
+    }
 
+    // Load internal plugins
     fs.readdirSync("./plugins").forEach((plugin) => {
-      if (path.extname(plugin).toLowerCase() == ".js") {
+      if (plugin.endsWith(".js")) {
         require("./plugins/" + plugin)
       }
     })
 
-    console.log(chalk.green.bold("✅ Plugins installed!"))
-    await conn.sendMessage(
-      conn.user.jid,
-      await startMessage(),
-      MessageType.text,
-      { detectLinks: false }
-    )
+    console.log(chalk.green.bold("✅ All plugins initialized!"))
+    await conn.sendMessage(conn.user.jid, await startMessage(), MessageType.text, { detectLinks: false })
   })
-  conn.on("close", (e) => console.log(e.reason))
 
+  conn.on("close", (e) => console.log(chalk.red(`Connection closed: ${e.reason}`)))
+
+  // Background Tasks
   await groupMuteSchuler(conn)
   await groupUnmuteSchuler(conn)
   await customMessageScheduler(conn)
 
+  // Handle Messages
   conn.on("chat-update", (m) => {
     if (!m.hasNewMessage) return
-    if (!m.messages && !m.count) return
-    const { messages } = m
-    const all = messages.all()
-    handleMessages(all[0], conn)
+    if (!m.messages || !m.count) return
+    handleMessages(m.messages.all()[0], conn)
   })
 
   try {
     await conn.connect()
   } catch (e) {
-    if (!nodb) {
-      console.log(chalk.red.bold("Eski sürüm stringiniz yenileniyor..."))
+    if (!noDbSession) {
+      console.log(chalk.red("Retrying login with saved session..."))
       conn.loadAuthInfo(Session.deCrypt(config.SESSION))
       try {
         await conn.connect()
-      } catch (e) {
-        return
+      } catch {
+        console.log("Failed to reconnect.")
       }
-    } else console.log(`${e.message}`)
+    } else {
+      console.log(`❌ Login error: ${e.message}`)
+    }
   }
 }
 
+// Boot Aura Bot
 ;(async () => {
   await prepareGreetingMedia()
-  whatsAsena(await waWebVersion())
+  auraBot(await waWebVersion())
 })()
